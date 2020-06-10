20a8ec0e04c5c222a0b7b3136b24b68b9ad50118
P. Mohan <pmverma.cs@gmail.com>
2020-06-10 04:21:55 +0000

    added tests

--- test/app.spec.js
+++ test/app.spec.js
@@ -15,13 +15,60 @@ describe('JHipster generator hypersistence-optimizer', () => {
                     testmode: true
                 })
                 .withPrompts({
-                    message: 'simple message to say hello'
+                    useHo: false
                 })
                 .on('end', done);
         });
+        it('should not generate configuration files', () => {
+            assert.noFile([
+                'src/main/resources/META-INF/services/org.hibernate.boot.spi.SessionFactoryBuilderFactory',
+                'src/main/java/com/mycompany/myapp/config/HypersistenceConfiguration.java'
+            ]);
+        });
+    });
 
-        it('generate dummy.txt file', () => {
-            assert.file(['dummy-maven.txt', 'dummy-angularX.txt']);
+    describe('Test with Maven and AngularX', () => {
+        beforeEach(done => {
+            helpers
+                .run(path.join(__dirname, '../generators/app'))
+                .inTmpDir(dir => {
+                    fse.copySync(path.join(__dirname, '../test/templates/maven-angularX'), dir);
+                })
+                .withOptions({
+                    testmode: true
+                })
+                .withPrompts({
+                    useHo: true
+                })
+                .on('end', done);
+        });
+        it('should generate configuration files', () => {
+            assert.file([
+                'src/main/resources/META-INF/services/org.hibernate.boot.spi.SessionFactoryBuilderFactory',
+                'src/main/java/com/mycompany/myapp/config/HypersistenceConfiguration.java'
+            ]);
+        });
+    });
+    describe('Test with Gradle and React', () => {
+        beforeEach(done => {
+            helpers
+                .run(path.join(__dirname, '../generators/app'))
+                .inTmpDir(dir => {
+                    fse.copySync(path.join(__dirname, '../test/templates/gradle-react'), dir);
+                })
+                .withOptions({
+                    testmode: true
+                })
+                .withPrompts({
+                    useHo: false
+                })
+                .on('end', done);
+        });
+        it('should not generate configuration files', () => {
+            assert.noFile([
+                'src/main/resources/META-INF/services/org.hibernate.boot.spi.SessionFactoryBuilderFactory',
+                'src/main/java/com/mycompany/myapp/config/HypersistenceConfiguration.java'
+            ]);
         });
     });
 
@@ -36,13 +83,15 @@ describe('JHipster generator hypersistence-optimizer', () => {
                     testmode: true
                 })
                 .withPrompts({
-                    message: 'simple message to say hello'
+                    useHo: true
                 })
                 .on('end', done);
         });
-
-        it('generate dummy.txt file', () => {
-            assert.file(['dummy-gradle.txt', 'dummy-react.txt']);
+        it('should generate configuration files', () => {
+            assert.file([
+                'src/main/resources/META-INF/services/org.hibernate.boot.spi.SessionFactoryBuilderFactory',
+                'src/main/java/com/mycompany/myapp/config/HypersistenceConfiguration.java'
+            ]);
         });
     });
 });
